975ebe3a4962de96848086ef5b7830ea17e30c49
fredd-for <fredd_for@hotmail.com>
2015-06-16 08:39:57 -0400

    cambios

--- public/scripts/productos/galeria.js
+++ public/scripts/productos/galeria.js
@@ -13,26 +13,13 @@ $(document).ready(function (){
 // 	});
 
 $("#file-1").fileinput({
-		// showUpload: false,
+		//showUpload: false,
 		// showCaption: false,
 		allowedFileExtensions : ['jpg','jpeg', 'png','gif'],
 		maxFileSize: 1000,
-		// browseClass: "btn btn-primary btn-lg",
+		browseClass: "btn btn-primary btn-sm",
 		// fileType: "any",
   //       previewFileIcon: "<i class='glyphicon glyphicon-king'></i>"
 });
 
-// $("#file-1").fileinput({
-//         uploadUrl: '#', // you must set a valid URL here else you will get an error
-//         allowedFileExtensions : ['jpg', 'png','gif'],
-//         overwriteInitial: false,
-//         maxFileSize: 1000,
-//         maxFilesNum: 10,
-//         //allowedFileTypes: ['image', 'video', 'flash'],
-//         slugCallback: function(filename) {
-//             return filename.replace('(', '_').replace(']', '_');
-//         }
-//     });
 
-
-})
