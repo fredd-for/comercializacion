9e2c1cb5db93ff9733f39002139d6e77a4e2b9d6
fredd-for <fredd_for@hotmail.com>
2016-01-27 19:50:30 -0400

    cam

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -28,7 +28,7 @@ class ControllerBase extends Controller {
             $user_id = $auth['id'];
             $this->_user = usuarios::findFirst("id = '$user_id'");
             //Prepend the application name to the title
-            $this->tag->setTitle('Sistema de Comercialización');
+            $this->tag->setTitle('Sistema de Contrataciones');
             //menu
             $this->menu($this->_user->nivel);
             $this->view->setVar('user', $this->_user);
