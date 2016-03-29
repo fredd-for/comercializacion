2c2a3d795928e93e1156879a8c00f104b4e763b1
fredd-for <fredd_for@hotmail.com>
2016-03-29 14:29:20 -0400

    prueba carlos

--- app/models/Facturas.php
+++ app/models/Facturas.php
@@ -8,6 +8,20 @@ class Facturas extends \Phalcon\Mvc\Model
 	 private $_db;
 	public function lista($where1,$group)
 	{
+		// $sql = "SELECT cl.razon_social,cl.correo,cl.representante_legal,cl.correo_representante_legal,cl.nombre_ref,cl.correo_ref,cl.nit,g.grupo,l.linea,e.estacion,c.contrato,p.producto,
+		// pp.*,IF( DATEDIFF(CURRENT_DATE(),pp.fecha_programado) >0, DATEDIFF(CURRENT_DATE(),pp.fecha_programado),0) AS diferencia_dias,ppf.nro_factura
+		// FROM planpagos pp
+		// INNER JOIN contratosproductos cp ON pp.contratoproducto_id = cp.id AND cp.baja_logica =1
+		// LEFT JOIN planpagofacturas ppf ON pp.id=ppf.planpago_id AND ppf.baja_logica = 1
+		// INNER JOIN productos p ON pp.producto_id = p.id
+		// INNER JOIN contratos c ON cp.contrato_id = c.id
+		// INNER JOIN clientes cl ON c.cliente_id = cl.id
+		// INNER JOIN grupos g ON p.grupo_id = g.id
+		// INNER JOIN estaciones e ON p.estacion_id = e.id
+		// INNER JOIN lineas l ON e.linea_id = l.id
+		// WHERE planpago_id IS NULL AND DATEDIFF(CURRENT_DATE(),pp.fecha_programado) >='-10' AND pp.baja_logica =1 ".$where1."
+		// ORDER BY c.contrato DESC ".$group;
+
 		$sql = "SELECT cl.razon_social,cl.correo,cl.representante_legal,cl.correo_representante_legal,cl.nombre_ref,cl.correo_ref,cl.nit,g.grupo,l.linea,e.estacion,c.contrato,p.producto,
 		pp.*,IF( DATEDIFF(CURRENT_DATE(),pp.fecha_programado) >0, DATEDIFF(CURRENT_DATE(),pp.fecha_programado),0) AS diferencia_dias,ppf.nro_factura
 		FROM planpagos pp
@@ -19,7 +33,7 @@ class Facturas extends \Phalcon\Mvc\Model
 		INNER JOIN grupos g ON p.grupo_id = g.id
 		INNER JOIN estaciones e ON p.estacion_id = e.id
 		INNER JOIN lineas l ON e.linea_id = l.id
-		WHERE planpago_id IS NULL AND DATEDIFF(CURRENT_DATE(),pp.fecha_programado) >='-10' AND pp.baja_logica =1 ".$where1."
+		WHERE planpago_id IS NULL AND pp.baja_logica =1 ".$where1."
 		ORDER BY c.contrato DESC ".$group;
 		$this->_db = new Facturas();
 		return new Resultset(null, $this->_db, $this->_db->getReadConnection()->query($sql));
@@ -38,6 +52,7 @@ class Facturas extends \Phalcon\Mvc\Model
 		INNER JOIN lineas l ON e.linea_id = l.id
 		WHERE planpago_id IS NOT NULL AND DATEDIFF(CURRENT_DATE(),pp.fecha_programado) >='-10' ".$where1."
 		ORDER BY DATEDIFF(CURRENT_DATE(),pp.fecha_programado) DESC ".$group;
+
 		$this->_db = new Facturas();
 		return new Resultset(null, $this->_db, $this->_db->getReadConnection()->query($sql));
 	}
